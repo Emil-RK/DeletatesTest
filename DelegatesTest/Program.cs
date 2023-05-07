250ae28ca3c1a1efe405dfe0b1deaad4ac256bef
Emil-RK <tetenen@gmail.com>
2023-05-07 15:03:16 +0300

    initial commit

---
+++ DelegatesTest/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DelegatesTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public delegate void Test(string text);
+    }
+}
